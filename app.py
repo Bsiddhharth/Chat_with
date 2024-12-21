3b8c2537f663d9aa85dae83380c48b92117315f5
bsiddhharth <bsiddhharth@gmail.com>
2024-12-21 23:20:55 +0530

    added app.py and  styles

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
-import chatbot.py
+import streamlit as st
+import chatbot
+
 
 def main():
-    chat()
+   chatbot.chat()
 
 
+   
+# for user (You)
+st.markdown("""
+    <style>
+    .user-label {
+        color: #0066ff;
+        font-weight: bold;
+        font-size: 19px;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+# for Assistant
+st.markdown("""
+    <style>
+    .assistant-label {
+        color: #FF4B4B;
+        font-weight: bold;
+        font-size: 18px;
+    }
+    </style>
+""", unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
--- chatbot.py
+++ chatbot.py
@@ -183,27 +183,7 @@ def chat():
                 st.markdown(f"<span class='assistant-label'>Assistant:</span> {chat['assistant']}", unsafe_allow_html=True)
 
 
-# for user (You)
-st.markdown("""
-    <style>
-    .user-label {
-        color: #0066ff;
-        font-weight: bold;
-        font-size: 19px;
-    }
-    </style>
-""", unsafe_allow_html=True)
 
-# for Assistant
-st.markdown("""
-    <style>
-    .assistant-label {
-        color: #FF4B4B;
-        font-weight: bold;
-        font-size: 18px;
-    }
-    </style>
-""", unsafe_allow_html=True)
 
 
 
