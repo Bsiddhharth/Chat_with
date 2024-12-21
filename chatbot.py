4a3eff63b53181f7ebb05fba456cdeaa84fff90f
bsiddhharth <bsiddhharth@gmail.com>
2024-12-21 23:43:33 +0530

    updated  requirements.txt

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,8 @@
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain # combining the entire doc and send it to the context
-from langchain_chroma import Chroma
+# from langchain_chroma import Chroma
 from langchain_community.chat_message_histories import ChatMessageHistory
-from langchain_core.chat_history import BaseChatMessageHistory
+# from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_groq import ChatGroq
 from langchain_core.runnables.history import RunnableWithMessageHistory
